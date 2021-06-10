de477fb8f57095ff0ea807d5e7b37d11c898b196
jeonsoonjo <tinasoonj@naver.com>
2021-06-10 10:05:18 +0900

    0610-update

---
+++ workspace/01_BEAN/src/main/java/ex04_xml/Calculator.java
@@ -0,0 +1,26 @@
+package ex04_xml;
+
+public class Calculator {
+
+
+	// property
+	
+	// constructor
+	
+	// method
+	public double add(double a, double b) {
+		return a + b;
+	}
+	public double subtract(double a, double b) {
+		return a - b;
+	}
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+	public double divide(double a, double b) {
+		return a / b;
+	}
+	
+}
+
+
