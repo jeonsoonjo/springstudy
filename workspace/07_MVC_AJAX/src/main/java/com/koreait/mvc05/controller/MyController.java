e0a1043f67c068c155d9bf876caae8d8259f368c
jeonsoonjo <tinasoonj@naver.com>
2021-06-16 09:54:02 +0900

    0616-update

--- workspace/07_MVC_AJAX/src/main/java/com/koreait/mvc05/controller/MyController.java
+++ workspace/07_MVC_AJAX/src/main/java/com/koreait/mvc05/controller/MyController.java
@@ -1,8 +1,5 @@
 package com.koreait.mvc05.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +19,7 @@ public class MyController {
 	// 1. String 반환하기
 	@RequestMapping(value="v01",
 					method=RequestMethod.GET,
-					produces="text/plain")
+					produces="text/plain; charset=utf-8")
 	@ResponseBody // ajax 처리를 위한 필수 애너테이션이다
 	public String v01(@RequestParam("name") String name,
 			   		  @RequestParam("age") int age) {
@@ -30,17 +27,19 @@ public class MyController {
 		return name + ", " + age;
 	}
 	
+	// 2. json 반환하기
 	@RequestMapping(value="v02",
 					method=RequestMethod.GET,
 					produces="application/json; charset=utf-8")
-	@ResponseBody
+	@ResponseBody // ajax 처리를 위한 필수 애너테이션이다
 	public Person v02(@RequestParam("name") String name,
 					  @RequestParam("age") int age){
 		Person p = new Person();
 		p.setName(name);
 		p.setAge(age);
 		
-		return p;
+		return p; // bean을 반환한다. produces="application/json; charset=utf-8"
+		// return은 ViewResolver에 의해서 jsp로 처리가 되는데
 	}
 	
 	
