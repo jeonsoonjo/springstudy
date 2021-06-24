1b3d6052810f5b9a84107d149b8ed7a45a2c3970
jeonsoonjo <tinasoonj@naver.com>
2021-06-24 17:47:09 +0900

    0624-update

---
+++ workspace/15_MEMBER/src/main/java/com/koreait/member/controller/MemberController.java
@@ -0,0 +1,73 @@
+package com.koreait.member.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.koreait.member.command.EmailAuthCommand;
+import com.koreait.member.command.IdCheckCommand;
+
+@Controller
+public class MemberController {
+
+	// field
+	private SqlSession sqlSession;
+	private IdCheckCommand idCheckCommand;
+	private EmailAuthCommand emailAuthCommand;
+	
+	// constructor
+	public MemberController(SqlSession sqlSession,
+							IdCheckCommand idCheckCommand,
+							EmailAuthCommand emailAuthCommand) {
+		super();
+		this.sqlSession = sqlSession;
+		this.idCheckCommand = idCheckCommand;
+		this.emailAuthCommand = emailAuthCommand;
+	}
+	
+	// method
+	@GetMapping(value="/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping(value="joinPage.do")
+	public String joinPage() {
+		return "member/join";
+	}
+	
+	@GetMapping(value="idCheck.do",
+				produces="application/json; charset=utf-8")
+	@ResponseBody
+	public Map<String, Integer> idCheck(HttpServletRequest request,
+					   					Model model) {
+		model.addAttribute("request", request);
+		return idCheckCommand.execute(sqlSession, model);
+	}
+	
+	@GetMapping(value="verifyNum.do",
+				produces="application/json; charset=utf-8")
+	@ResponseBody
+	public Map<String, String> verifyNum(HttpServletRequest request,
+										 Model model) {
+		model.addAttribute("request", request);
+		return emailAuthCommand.execute(sqlSession, model);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
--- workspace/15_MEMBER/src/main/webapp/WEB-INF/views/index.jsp
+++ workspace/15_MEMBER/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,13 +1,40 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Insert title here</title>
+	<title>홈페이지</title>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<script>
+		$(document).ready(function(){
+			fn_login();
+		});
+		function fn_login(){
+			$('#f').submit(function(e){
+				if($('#id').val() == '' || $('#pw').val() == ''){
+					alert('아이디와 비밀번호는 필수입니다.');
+					e.preventDefault();
+					return false;
+				}
+			})
+		}
+	</script>
 </head>
 <body>
 
+	<h1>홈페이지</h1>
+	
+	<form action="login.do" id="f" method="post">
+		아이디<br>
+		<input type="text" name="id" id="id" placeholder="ID"><br><br>
+		비밀번호<br>
+		<input type="password" name="pw" id="pw" placeholder="Password"><br><br>
+		<button>로그인</button>
+		<a href="joinPage.do">회원가입</a>
+	</form>
+	
 </body>
 </html>
 
