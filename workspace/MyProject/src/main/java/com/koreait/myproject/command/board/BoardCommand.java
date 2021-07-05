63cba3d48fbd9ba1a8002fcc11de002edf4ce91f
jeonsoonjo <tinasoonj@naver.com>
2021-07-05 17:48:23 +0900

    0705-update

--- workspace/MyProject/src/main/java/com/koreait/myproject/command/board/BoardCommand.java
+++ workspace/MyProject/src/main/java/com/koreait/myproject/command/board/BoardCommand.java
@@ -3,7 +3,7 @@ package com.koreait.myproject.command.board;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.ui.Model;
 
-public interface ImgBoardCommand {
+public interface BoardCommand {
 
 	public void execute(SqlSession sqlSession, Model model);
 
---
+++ workspace/MyProject/src/main/java/com/koreait/myproject/command/board/SelectImgBoardListCommand.java
@@ -0,0 +1,16 @@
+package com.koreait.myproject.command.board;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+public class SelectImgBoardListCommand implements BoardCommand {
+
+	@Override
+	public void execute(SqlSession sqlSession, Model model) {
+		
+		
+
+	}
+
+}
+
